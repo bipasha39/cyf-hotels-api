5cda8d1c23d2090d5579fa627e18f2a4b4c0b8f2
Leander Rysanek <leander.rysanek@derivco.se>
2020-12-15 14:03:47 +0100

    Restructure migrations

--- migrations/init.js
+++ migrations/init.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const { pool } = require("../db.js");
 
 pool
-  .query(fs.readFileSync("migrations/0_seed_data.sql").toString())
+  .query(fs.readFileSync("migrations/0_initial_snapshot.sql").toString())
   .catch((e) => {
     console.error(e);
     process.exit(1);
